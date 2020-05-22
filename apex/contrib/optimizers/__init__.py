2ff832ad19342a1160dbec5d8d101536ec489297
Edward Hu <j.edw.hu@gmail.com>
2020-05-22 20:52:12 +0000

    enable DFA

--- apex/contrib/optimizers/__init__.py
+++ apex/contrib/optimizers/__init__.py
@@ -1,4 +1,6 @@
 from .fp16_optimizer import FP16_Optimizer
 from .fused_adam import FusedAdam
 from .distributed_fused_adam import DistributedFusedAdam
+from .distributed_fused_adam_v2 import DistributedFusedAdamV2
+from .distributed_fused_adam_v3 import DistributedFusedAdamV3
 from .fused_lamb import FusedLAMB
